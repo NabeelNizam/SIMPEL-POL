d9eb53e32cf471aed0e13a12ef6e41f2cac9228c
VozSoldat <muhamaderril7@gmail.com>
2025-05-30 20:23:01 +0700

    feat: implement user data CRUD
    commit 6900e93cede198791f0fa53d5d963e2739c28571
    Author: VozSoldat <muhamaderril7@gmail.com>
    Date:   Fri May 30 20:22:49 2025 +0700
    
        feat: implement edit and delete user data
    
    commit ca6dd93495026bc2572c97c4622643e69fcd9361
    Author: VozSoldat <muhamaderril7@gmail.com>
    Date:   Fri May 30 13:52:48 2025 +0700
    
        continue work on user detail
    
    commit 8da80ba45af5bfa867351913fc18c789afdda37e
    Author: VozSoldat <muhamaderril7@gmail.com>
    Date:   Fri May 30 13:49:40 2025 +0700
    
        feat: add `create user`

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,6 +10,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Yajra\DataTables\Facades\DataTables;
 
 
@@ -31,61 +32,61 @@ class AdminController extends Controller
 
         return view('admin.dashboard', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
     }
-public function pengguna(Request $request)
-{
-    $breadcrumb = (object) [
-        'title' => 'Manajemen Pengguna',
-        'list' => ['Home', 'Manajemen Data Pengguna']
-    ];
+    public function pengguna(Request $request)
+    {
+        $breadcrumb = (object) [
+            'title' => 'Manajemen Pengguna',
+            'list' => ['Home', 'Manajemen Data Pengguna']
+        ];
 
-    $page = (object) [
-        'title' => 'Daftar pengguna yang terdaftar dalam sistem'
-    ];
+        $page = (object) [
+            'title' => 'Daftar pengguna yang terdaftar dalam sistem'
+        ];
 
-    $activeMenu = 'pengguna';
+        $activeMenu = 'pengguna';
 
-    $role = Role::all();
-    $query = User::with('role');
+        $role = Role::all();
+        $query = User::with('role');
 
-    // Filter berdasarkan role
-    if ($request->id_role) {
-        $query->where('id_role', $request->id_role);
+        // Filter berdasarkan role
+        if ($request->id_role) {
+            $query->where('id_role', $request->id_role);
+        }
+
+        // Filter berdasarkan pencarian
+        if ($request->search) {
+            $query->where(function ($q) use ($request) {
+                $q->where('nama', 'like', "%{$request->search}%")
+                    ->orWhere('email', 'like', "%{$request->search}%");
+            });
+        }
+
+        // Sorting
+        $sortColumn = $request->sort_column ?? 'nama'; // Default sorting by nama
+        $sortDirection = $request->sort_direction ?? 'asc'; // Default sorting direction
+        $query->orderBy($sortColumn, $sortDirection);
+
+        $perPage = $request->input('per_page', 10); // default 10
+        $users = $query->paginate($perPage);
+
+        $users->appends(request()->query());
+
+
+        if ($request->ajax()) {
+            $html = view('admin.pengguna.user_table', compact('users'))->render();
+            return response()->json(['html' => $html]);
+        }
+
+        return view('admin.pengguna.user', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'activeMenu' => $activeMenu,
+            'role' => $role,
+            'users' => $users,
+        ]);
     }
-
-    // Filter berdasarkan pencarian
-    if ($request->search) {
-        $query->where(function ($q) use ($request) {
-            $q->where('nama', 'like', "%{$request->search}%")
-              ->orWhere('email', 'like', "%{$request->search}%");
-        });
-    }
-
-    // Sorting
-    $sortColumn = $request->sort_column ?? 'nama'; // Default sorting by nama
-    $sortDirection = $request->sort_direction ?? 'asc'; // Default sorting direction
-    $query->orderBy($sortColumn, $sortDirection);
-
-    $perPage = $request->input('per_page', 10); // default 10
-    $users = $query->paginate($perPage);
-
-    $users->appends(request()->query());
-    
-
-    if ($request->ajax()) {
-    $html = view('admin.pengguna.user_table', compact('users'))->render();
-    return response()->json(['html' => $html]);
-}
-
-    return view('admin.pengguna.user', [
-    'breadcrumb' => $breadcrumb,
-    'page' => $page,
-    'activeMenu' => $activeMenu,
-    'role' => $role,
-    'users' => $users,
-]);
-
-}
-    public function create_ajax(){
+    public function create_ajax()
+    {
         $role = Role::all();
         $jurusan = Jurusan::all();
         return view('admin.pengguna.create', [
@@ -93,46 +94,121 @@ public function pengguna(Request $request)
             'jurusan' => $jurusan
         ]);
     }
-    public function edit_ajax($id){
+    public function edit_ajax(User $user)
+    {
         $role = Role::all();
-         $user = User::findOrFail($id);
         $jurusan = Jurusan::all();
         return view('admin.pengguna.edit', [
-        'role' => $role,
-        'jurusan' => $jurusan, 'user' => $user
-    ]);
+            'role' => $role,
+            'jurusan' => $jurusan,
+            'user' => $user
+        ]);
     }
-    public function show_ajax($id)
-{
-    $role = Role::all();
-         $user = User::findOrFail($id);
+    public function update_ajax(Request $request, User $user)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'nama' => [
+                    'required',
+                    'string',
+                    'max:255',
+                ],
+                'telepon' => [
+                    'required',
+                    'string',
+                    'max:15',
+                ],
+                'email' => [
+                    'required',
+                    'email',
+                    Rule::unique('users', 'email')->ignore($user->id_user, 'id_user'),
+                ],
+                'username' => [
+                    'required',
+                    'string',
+                    'max:50',
+                    Rule::unique('users', 'username')->ignore($user->id_user, 'id_user'),
+                ],
+                'password' => [
+                    'nullable',
+                    'string',
+                    'min:8',
+                ],
+                'jurusan' => [
+                    'nullable',
+                    'integer',
+                    'exists:jurusan,id_jurusan',
+                ],
+                'role' => [
+                    'nullable',
+                    'integer',
+                    'exists:roles,id_role',
+                ],
+            ]);
+
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'errors' => 'weladalah '.$validator->errors()
+                ]);
+                // return redirect()->route('admin.pengguna', $user)->withErrors($validator)->withInput();
+            }
+
+            $new_data = [
+                'nama' => $request->nama,
+                'telepon' => $request->telepon,
+                'email' => $request->email,
+                'username' => $request->username,
+                'id_jurusan' => $request->id_jurusan,
+                'id_role' => $request->id_role
+            ];
+            if ($request->password) {
+                $new_data['password'] = Hash::make($request->password);
+            }
+            $user->update($new_data);
+
+            return redirect()->route('admin.pengguna')->with('success', 'Pengguna berhasil diperbarui.');
+        } catch (\Exception $e) {
+            // return redirect()->route('admin.pengguna')->with('error', 'Terjadi kesalahan: ' . $e->getMessage());
+            return response()->json([
+                'success' => false,
+                'message' => 'Terjadi kesalahan: ' . $e->getMessage()
+            ]);
+        }
+    }
+    public function show_ajax(User $user)
+    {
+        $role = Role::all();
         $jurusan = Jurusan::all();
         return view('admin.pengguna.detail', [
-        'role' => $role,
-        'jurusan' => $jurusan, 'user' => $user]);
-}
-    public function import_ajax(){
+            'role' => $role,
+            'jurusan' => $jurusan,
+            'user' => $user
+        ]);
+    }
+    public function import_ajax()
+    {
         return view('admin.pengguna.import');
     }
-    public function confirm_ajax(){
-        return view('admin.pengguna.confirm');
-    }
 
-   public function store_ajax(Request $request)
-{
-    // Validasi input umum
-    $rules = [
-        'nama' => 'required|string|max:255',
-        'telepon' => 'required|string|max:15',
-        'email' => 'required|email|unique:users,email',
-        'username' => 'required|string|max:50|unique:users,username',
-        'jurusan' => 'required|integer|exists:jurusan,id_jurusan',
-        'id_role' => 'required|integer|exists:roles,id_role',
-        'password' => 'required|string|min:6',
-    ];
+    public function store_ajax(Request $request)
+    {
+        // Validasi input umum
+        $rules = [
+            'nama' => 'required|string|max:255',
+            'telepon' => 'required|string|max:15',
+            'email' => 'required|email|unique:users,email',
+            'username' => 'required|string|max:50|unique:users,username',
+            'jurusan' => 'required|integer|exists:jurusan,id_jurusan',
+            'id_role' => 'required|integer|exists:roles,id_role',
+            'password' => 'required|string|min:6',
+        ];
 
         // Cek apakah role Mahasiswa
-        $isMahasiswa = strtolower(Role::find($request->id_role)->nama_role) === 'mahasiswa';
+        // $isMahasiswa = strtolower(Role::find($request->id_role)->nama_role) === 'mahasiswa';
+        $isMahasiswa = Role::find($request->id_role)->kode_role === 'MHS';
 
         // Tambahkan validasi nim atau nip sesuai role
         if ($isMahasiswa) {
@@ -164,41 +240,48 @@ public function pengguna(Request $request)
             ]);
 
             if ($isMahasiswa) {
-                $identifier = Mahasiswa::create([
+                Mahasiswa::create([
                     'id_user' => $user->id_user,
                     'nim' => $request->identifier
                 ]);
             } else {
                 Pegawai::create([
+                    'nip' => $request->identifier,
                     'id_user' => $user->id_user,
-                    'nip' => $request->identifier
                 ]);
             }
 
-            return response()->json([
-                'success' => true,
-                'message' => 'Pengguna berhasil ditambahkan.'
-            ]);
+            // return response()->json([
+            //     'success' => true,
+            //     'message' => 'Pengguna berhasil ditambahkan.'
+            // ]);
+            return redirect()->route('admin.pengguna')->with('success', 'Pengguna berhasil ditambahkan.');
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'Terjadi kesalahan: ' . $e->getMessage()
             ], 500);
+            // return redirect()->route('admin.pengguna')->with('error', 'Terjadi kesalahan: ' . $e->getMessage());
         }
     }
 
+    public function confirm_ajax(User $user)
+    {
+        return view('admin.pengguna.confirm')->with([
+            'user' => $user
+        ]);
+    }
 
-    public function remove_ajax($id)
+    public function remove_ajax(User $user)
     {
         try {
-            $user = User::findOrFail($id);
-
             $user->delete();
 
-            return response()->json([
-                'success' => true,
-                'message' => 'Pengguna berhasil dihapus.'
-            ]);
+            return redirect()->route('admin.pengguna')->with('success', 'Pengguna berhasil dihapus.');
+            // return response()->json([
+            //     'success' => true,
+            //     'message' => 'Pengguna berhasil dihapus.'
+            // ]);
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
