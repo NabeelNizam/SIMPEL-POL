081ca454d19ac06568fe8d858d3a94883e624bc9
VozSoldat <muhamaderril7@gmail.com>
2025-06-09 18:21:41 +0700

    chore(pelapor/aduan): add checking so Aduan creation status is following the other older Aduans

--- app/Models/Periode.php
+++ app/Models/Periode.php
@@ -18,7 +18,8 @@ class Periode extends Model
         'tanggal_mulai',
         'tanggal_selesai'
     ];
-    
+    protected $appends = ['periode_aktif'];
+
     public static function getPeriodeAktif()
     {
         $tanggalSekarang = Carbon::now();
